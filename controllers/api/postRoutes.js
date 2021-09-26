d17b82f72b6a20034824da19f72d665b7b03b541
Zakk Fast <zakkfastpro@gmail.com>
2021-09-25 20:40:23 -0700

    finished post routes

---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,103 @@
+const { Post, User, Comment } = require("../../models");
+const sequelize = require("../../config/connection");
+const withAuth = require("../../utils/auth");
+const router = require("express").Router();
+
+router.get("/", (req, res) => {
+  console.log("======================");
+  Post.findAll({
+    attributes: ["id", "subject_name", "body_content", "date_created"],
+    order: [["created_at", "DESC"]],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "content", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["user_name", "github"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["user_name", "github"],
+      },
+    ],
+  })
+    .then((dbPostData) => res.json(dbPostData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.post("/", withAuth, (req, res) => {
+  Post.create({
+    subject_name: req.body.subject_name,
+    body_content: req.body.body_content,
+    user_id: req.session.user_id,
+  })
+    .then((dbPostData) => res.json(dbPostData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get("/:id", (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "subject_name", "post_content", "date_created"],
+    include: [
+      {
+        model: User,
+        attributes: ["user_name", "github"],
+      },
+      {
+        model: Comment,
+        attributes: ["id", "content", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["user_name", "github"],
+        },
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.put("/:id", withAuth, (req, res) => {
+  Post.update(
+    {
+      subject_name: req.body.subject_name,
+      body_content: req.body.body_content,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
