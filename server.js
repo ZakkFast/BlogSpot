720531c03a00452759a64dd3bfad0aecdd8f37a2
Zakk Fast <zakkfastpro@gmail.com>
2021-09-26 02:37:24 -0700

    removed server port

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT;
 
+
 const hbs = exphbs.create({ helpers });
 
 const sess = {
@@ -31,6 +32,8 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('port', PORT)
 
 app.use(routes);
 
