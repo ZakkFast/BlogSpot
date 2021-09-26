ad4bc0788ad486e957570eb17d3797c6891aae90
Zakk Fast <zakkfastpro@gmail.com>
2021-09-26 14:47:26 -0700

    seperated login in and signup functions

--- public/js/login.js
+++ public/js/login.js
@@ -22,39 +22,8 @@ async function loginFormHandler(event) {
   }
 }
 
-async function signupFormHandler(event) {
-  event.preventDefault();
 
-  const user_name = document.querySelector('#name-signup').value.trim();
-  const email = document.querySelector('#email-signup').value.trim();
-  const pwd = document.querySelector('#password-signup').value.trim();
-  const github = document.querySelector('#github-signup').value.trim();
-
-  if (user_name && email && pwd) {
-    const response = await fetch('/api/users', {
-      method: 'post',
-      body: JSON.stringify({
-        user_name,
-        email,
-        github,
-        pwd
-      }),
-      headers: { 'Content-Type': 'application/json' }
-    });
-
-    // check the response status
-    if (response.ok) {
-      console.log('success');
-      document.location.replace('/dashboard');
-    } else {
-      alert(response.statusText);
-    }
-  }
-}
   document
     .querySelector('.login-form')
     .addEventListener('submit', loginFormHandler);
   
-  document
-    .querySelector('.signup-form')
-    .addEventListener('submit', signupFormHandler);
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001;
 
 
 const hbs = exphbs.create({ helpers });
@@ -33,7 +33,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.set('port', PORT)
 
 app.use(routes);
 
